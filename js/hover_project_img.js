db0b04ed932df0f31f5f23fbb3bc1fdf53854174
Marc Michel <marcmichel117@gmail.com>
2020-12-05 15:48:38 +0100

    added responsive design for projects

--- js/hover_project_img.js
+++ js/hover_project_img.js
@@ -1,24 +1,47 @@
 screenTooSmall = 0;
+var allElementsToReset = [$(".project-overline"),
+$(".project-title"),
+$(".project-description"),
+$(".project-informations"),
+$(".project-image")];
 
 jQuery(window).resize(function () {
     var windowsize = jQuery(window).width();
-    if (windowsize >= 991) {
+    if (windowsize >= 990) {
         screenTooSmall = false;
     } else {
         screenTooSmall = true;
     }
 });
 
-
 $(function () {
     boxRollovers();
 });
 
+$(document).ready(function () {
+    var windowsize = jQuery(window).width();
+    console.log(windowsize);
+    if (windowsize > 976) {
+        screenTooSmall = false;
+    } else {
+        screenTooSmall = true;
+    }
+
+    if (screenTooSmall) {
+        var image = $(this).children('.project-image').children('a').children('img');
+        image.css({ 'filter': 'none', '-webkit-filter': 'none' });
+
+        var content = $(this).children('.project-content');
+        content.css({ 'filter': 'alpha(opacity=0)', 'opacity': '0' });
+    }
+});
+
 function boxRollovers() {
-    $selector = $(".project_container");
+    $selector = $(".project-card");
     XAngle = 0;
     YAngle = 0;
-    Z = 50;
+    //Z = 50;
+    //Z = 10;
 
     $selector.on("mousemove", function (e) {
         var $this = $(this);
@@ -29,14 +52,23 @@ function boxRollovers() {
         YAngle = -(0.5 - (XRel / width)) * 5;
         XAngle = (0.5 - (YRel / width)) * 5;
         if (!screenTooSmall) {
-            updateView($this.children(".project").children());
+            updateView($this.children(".project-image"), 50);
+            console.log($this.children());
+
+            updateView($this.children(".project-content").children(), 10);
+            /*
+            $.each(allContentElements, function (index, value) {
+                console.log(value);
+                updateView(value, 10);
+            }); 
+            */
+
         }
     });
 
     $selector.on("mouseleave", function () {
-        $(this).children(".project").children().each(function () {
-            oLayer = $(this);
-
+        $.each(allElementsToReset, function (index, value) {
+            oLayer = value;
             oLayer.css({ "transform": "perspective(525px) translateZ(0) rotateX(0deg) rotateY(0deg)", "transition": "all 150ms linear 0s", "-webkit-transition": "all 150ms linear 0s" });
             oLayer.find("strong").css({ "transform": "perspective(525px) translateZ(0) rotateX(0deg) rotateY(0deg)", "transition": "all 150ms linear 0s", "-webkit-transition": "all 150ms linear 0s" });
 
@@ -44,7 +76,48 @@ function boxRollovers() {
     });
 }
 
-function updateView(oLayer) {
-    oLayer.css({ "transform": "perspective(525px) translateZ(" + Z + "px) rotateX(" + XAngle + "deg) rotateY(" + YAngle + "deg)", "transition": "none", "-webkit-transition": "none" });
-    oLayer.find("strong").css({ "transform": "perspective(525px) translateZ(" + Z + "px) rotateX(" + (XAngle / 0.66) + "deg) rotateY(" + (YAngle / 0.66) + "deg)", "transition": "none", "-webkit-transition": "none" });
+function updateView(oLayer, z) {
+    oLayer.css({ "transform": "perspective(525px) translateZ(" + z + "px) rotateX(" + XAngle + "deg) rotateY(" + YAngle + "deg)", "transition": "none", "-webkit-transition": "none" });
+    oLayer.find("strong").css({ "transform": "perspective(525px) translateZ(" + z + "px) rotateX(" + (XAngle / 0.66) + "deg) rotateY(" + (YAngle / 0.66) + "deg)", "transition": "none", "-webkit-transition": "none" });
 }
+
+var hideContent = true;
+$('.project-card').click(function () {
+    hideContent = !hideContent;
+
+    if (screenTooSmall && !hideContent) {
+        var image = $(this).children('.project-image').children('a').children('img');
+        image.css({ 'filter': 'brightness(0.3) saturate(100%) blur(4px)', '-webkit-filter': 'brightness(0.3) saturate(100%) blur(4px)' });
+
+        var content = $(this).children('.project-content');
+        content.css({ 'filter': 'alpha(opacity=100)', 'opacity': '100' })
+    } else {
+        if (screenTooSmall && hideContent) {
+            var image = $(this).children('.project-image').children('a').children('img');
+            image.css({ 'filter': 'none', '-webkit-filter': 'none' });
+
+            var content = $(this).children('.project-content');
+            content.css({ 'filter': 'alpha(opacity=0)', 'opacity': '0' });
+        }
+    }
+});
+
+$('.project-card').hover(function () {
+    if (screenTooSmall && hideContent) {
+        var image = $(this).children('.project-image').children('a').children('img');
+        image.css({ 'filter': 'none', '-webkit-filter': 'none' });
+
+        var content = $(this).children('.project-content');
+        content.css({ 'filter': 'alpha(opacity=0)', 'opacity': '0' })
+    }
+}, function () {
+    hideContent = true;
+
+    if (screenTooSmall) {
+        var image = $(this).children('.project-image').children('a').children('img');
+        image.css({ 'filter': 'brightness(0.3) saturate(100%) blur(4px)', '-webkit-filter': 'brightness(0.3) saturate(100%) blur(4px)' });
+
+        var content = $(this).children('.project-content');
+        content.css({ 'filter': 'alpha(opacity=100)', 'opacity': '100' })
+    }
+});
