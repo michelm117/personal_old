5def7622ba2105efac5fa4f1f620406be4f690cb
Marc Michel <marcmichel117@gmail.com>
2020-12-06 16:28:47 +0100

    added success message after post

--- js/submit.js
+++ js/submit.js
@@ -1,12 +1,11 @@
 $('.contact-form').on('submit', function (e) {
-    alert("NICE");
     e.preventDefault(); //this stops the form submit + refresh 
 
     $.ajax({
         data: { name: $('#name').val(), mail: $('#email').val(), message: $('#message').val() },
-        type: 'post',
-        url: '../php/contactform.php',
-        success: function (r) { $('.success-message').removeClass('hidden') },
-        error: function (r) { alert('Message could not be send. Please write an email to <mail@marcmichel.dev>.'); console.log(r) }
+        type: 'POST',
+        url: './php/contactform.php',
+        success: function (data) { $('.success-message').removeClass('hidden') },
+        error: function (data) { alert('Message could not be send. Please write an email to <mail@marcmichel.dev>.' + data); console.log(r + " \n " + data) }
     });
 });
