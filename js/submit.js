f77e09b951fc1851814425f8c2a7ccd05997cf95
Marc Michel <marcmichel117@gmail.com>
2020-12-06 18:20:36 +0100

    updated about me

--- js/submit.js
+++ js/submit.js
@@ -5,7 +5,17 @@ $('.submit-btn').click('submit', function (e) {
         data: { name: $('#name').val(), mail: $('#email').val(), message: $('#message').val() },
         type: 'POST',
         url: './php/contactform.php',
-        success: function (data) { $('.success-message').removeClass('hidden'); console.log(data) },
-        error: function (data) { alert('Message could not be send. Please write an email to <mail@marcmichel.dev>.' + data); console.log(data) }
+        success: function (data) {
+            $('.success-message').removeClass('hidden');
+            $('.submit-btn').prop("disabled", true);
+            $('.submit-btn').css("background", "#474559");
+            console.log(data);
+        },
+        error: function (data) { 
+            $('.submit-btn').css("background", "#474559"); 
+            $('.submit-btn').prop("disabled", true); 
+            alert('Message could not be send. Please write an email to mail@marcmichel.dev.'); 
+            console.log(data);
+        }
     });
 });
