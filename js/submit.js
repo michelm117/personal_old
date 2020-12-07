a62ae768b2c4694d8937f22fa289d353aeb1851d
Marc Michel <marcmichel117@gmail.com>
2020-12-07 02:47:13 +0100

    added error message for contact fields

--- js/submit.js
+++ js/submit.js
@@ -1,5 +1,43 @@
+function indicateError(field) {
+    $(field).css("border", "1px solid red");
+    $(field).addClass("error");
+    setTimeout(function () {
+        $(field).removeClass('error');
+    }, 600);
+}
+
 $('.submit-btn').click('submit', function (e) {
     e.preventDefault(); //this stops the form submit + refresh 
+    //$('.success-message').removeClass('hidden');
+
+    var inputFields = ['#nameField', '#mailField', '#messageField'];
+
+    // Add error class if necessary.
+    var errorOccured = false;
+    $.each(inputFields, function (index, field) {
+        // CHeck if input fields are empty
+        if (!$(field).val()) {
+            errorOccured = true;
+            indicateError(field);
+        } else {
+            $(field).css("border", "1px solid #ced4da");
+        }
+    });
+
+    // validate email
+    var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
+    var userMail = $('#mailField').val();
+    if (!pattern.test(userMail)) {
+        indicateError('#mailField');
+        errorOccured = true;
+    } else {
+        $('#mailField').css("border", "1px solid #ced4da");
+    }
+
+
+    if (errorOccured) {
+        return;
+    }
 
     $.ajax({
         data: { name: $('#name').val(), mail: $('#email').val(), message: $('#message').val() },
@@ -7,14 +45,16 @@ $('.submit-btn').click('submit', function (e) {
         url: './php/contactform.php',
         success: function (data) {
             $('.success-message').removeClass('hidden');
+            $('.error-message').addClass('hidden');
+
             $('.submit-btn').prop("disabled", true);
             $('.submit-btn').css("background", "#474559");
             console.log(data);
         },
-        error: function (data) { 
-            $('.submit-btn').css("background", "#474559"); 
-            $('.submit-btn').prop("disabled", true); 
-            alert('Message could not be send. Please write an email to mail@marcmichel.dev.'); 
+        error: function (data) {
+            $('.submit-btn').css("background", "#474559");
+            $('.error-message').removeClass('hidden');
+            $('.submit-btn').prop("disabled", true);
             console.log(data);
         }
     });
--- php/contactform.php
+++ php/contactform.php
@@ -6,9 +6,10 @@ $mailFrom = $_POST['mail'];
 $to = 'mail@marcmichel.dev';
 $message = $_POST['message'];
 $subject = 'Contact Request From Portfolio';
+$ip = $_SERVER['REMOTE_ADDR'];
 
 $headers = "From: ".$mailFrom;
-$txt = "You have received an e-mail from ".$name." <".$mailFrom.">.\n\n".$message;
+$txt = "You have received an e-mail from ".$name." <".$mailFrom.">.\nThe message was send from the following IP: ".$ip."\n\nMessage:".$message;
 
 mail($to, $subject, $txt, $headers);
 header("Location: ../index.html?mailsend");
