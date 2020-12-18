1a5b5fcdac666d780cf20b31d222c84533dd9362
Marc Michel <marcmichel117@gmail.com>
2020-12-18 01:59:11 +0100

    fixed mail bug

--- php/contactform.php
+++ php/contactform.php
@@ -9,7 +9,7 @@ $subject = 'Contact Request From Portfolio';
 $ip = $_SERVER['REMOTE_ADDR'];
 
 $headers = "From: ".$mailFrom;
-$txt = "You have received an e-mail from ".$name." <".$mailFrom.">.\nThe message was send from the following IP: ".$ip."\n\nMessage:".$message;
+$txt = "You have received an e-mail from ".$name." <".$mailFrom.">.\nThe message was send from the following IP: ".$ip."\n\nMessage:\n".$message;
 
 mail($to, $subject, $txt, $headers);
 header("Location: ../index.html?mailsend");
